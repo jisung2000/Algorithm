83c2c6307cc982c181596982327b44c41186735e
Jisung <129858557+jisung2000@users.noreply.github.com>
2025-05-05 11:10:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.69 ms, Memory: 38.1 MB -BaekjoonHub

--- Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,10 +2,7 @@ from collections import Counter
 import math
 
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    n = len(participant)-1
-    for i in range(len(completion)):
-        if(participant[i]!=completion[i]):
-            return participant[i]
-    return participant[n]
+    str = Counter(participant)- Counter(completion)
+    
+    return list(str.keys())[0]
+    
